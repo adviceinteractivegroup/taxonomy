c84207d731e477f3f3ebd9428150900b3f1fa768
harv <harvard.young@gmail.com>
2017-05-03 18:15:09 -0700

    simple taxonomy module

---
+++ index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+let csv = require('csvtojson');
+let _ = require('lodash');
+
+// simple find
+module.exports.find  = (gcid, directory, done) => {
+  let cats = [];
+  csv().fromFile('taxonomy.csv')
+  .on('json', data => {
+    cats.push(data);
+  })
+  .on('done', err => {
+    if (err) {
+      return done(err);
+    }
+
+    let google = _.filter(cats, {gcid: gcid});
+    if (google.length < 1) {
+      return done('could not find gcid')
+    }
+
+    if (_.get(google, `0.${directory}`)) {
+      return done(null, _.get(google, `0.${directory}`));
+    }
+    return done('no mapping for directory');
+  });
+};
