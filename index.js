da3a6bc5c627c98f40db8d499ace897f75db6b2f
Harvard Young <harvard.young@gmail.com>
2019-02-21 08:31:22 -0800

    Revert "Add citysquare column"

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 'use strict';
 
-const csv = require('csvtojson');
-const _ = require('lodash');
+let csv = require('csvtojson');
+let _ = require('lodash');
 
 // simple find
-const find = async (gcid, directory, done) => {
-  try {
-    const csvFilePath = `${__dirname}/taxonomy.csv`;
-    const cats = await csv().fromFile(csvFilePath);
+module.exports.find  = (gcid, directory, done) => {
+  let cats = [];
+  csv().fromFile(`${__dirname}/taxonomy.csv`)
+  .on('json', data => {
+    cats.push(data);
+  })
+  .on('done', err => {
+    if (err) {
+      return done(err);
+    }
+
     let google = _.filter(cats, {gcid: gcid});
     if (google.length < 1) {
       return done('could not find gcid')
     }
+
     if (_.get(google, `0.${directory}`)) {
       return done(null, _.get(google, `0.${directory}`));
     }
     return done('no mapping for directory');
-  } catch (error) {
-    return done(error);
-  };
-};
-
-module.exports = {
-  find
+  });
 };
